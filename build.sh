d1b197723e79b740c0498b28ca0ae43b698adcb1
naturecodevoid <44983869+naturecodevoid@users.noreply.github.com>
2023-01-08 10:47:29 -0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,9 +4,11 @@ cd packages/wrangler
 cp ../../.gitignore .
 npm i
 npm run build
+git config --global user.email ""
+git config --global user.name "github-actions"
 git init
 git add --all
 git commit -m "build"
 git branch -M wrangler-build
 git remote add origin https://github.com/naturecodevoid/wrangler2.git
-git push -u origin wrangler-build
+git push -f -u origin wrangler-build
