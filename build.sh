32ae42dceeae38194317518969d60290b1ec4a3b
naturecodevoid <44983869+naturecodevoid@users.noreply.github.com>
2023-01-08 11:13:33 -0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@
 npm i
 cd packages/wrangler
 npm run build
-git config --local user.email "41898282+github-actions[bot]@users.noreply.github.com"
-git config --local user.name "github-actions[bot]"
+git config --global user.email "41898282+github-actions[bot]@users.noreply.github.com"
+git config --global user.name "github-actions[bot]"
 git config --global init.defaultBranch wrangler-build
 git init
 git add --all
