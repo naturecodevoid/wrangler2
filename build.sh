54194471f754e426f9d6f23b3be3b31272fb1cd2
naturecodevoid <44983869+naturecodevoid@users.noreply.github.com>
2023-01-20 14:22:08 -0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 npm i
 cd packages/wrangler
-jq '.version += "-esbuild-plugins"' package.json > test
+jq '.version += "-esbuild-plugins"' package.json | sponge package.json
 npm run build
 git config --global user.email "41898282+github-actions[bot]@users.noreply.github.com"
 git config --global user.name "github-actions[bot]"
