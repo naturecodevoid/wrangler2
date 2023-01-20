41143bd8866c608b8d766b3ae1538e6b19319550
naturecodevoid <44983869+naturecodevoid@users.noreply.github.com>
2023-01-20 14:16:25 -0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 npm i
 cd packages/wrangler
-jq --raw-output '.version += "-esbuild-plugins"' package.json > package.json
+jq --raw-output '.version += "-esbuild-plugins"' package.json
 cat package.json
 npm run build
 git config --global user.email "41898282+github-actions[bot]@users.noreply.github.com"
